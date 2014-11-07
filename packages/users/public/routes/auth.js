5659346904060b30111aa711723eb0d7ee0b51d9
harssh <harsshs@sparkway.com>
2014-11-07 17:43:43 +0530

    Added routes for edit profile template ng

--- packages/users/public/routes/auth.js
+++ packages/users/public/routes/auth.js
@@ -23,6 +23,27 @@ angular.module('mean.users').config(['$stateProvider',
       return deferred.promise;
     };
 
+    // Check if the user is connected
+    var checkLoggedin = function($q, $timeout, $http, $location) {
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('/loggedin').success(function(user) {
+        // Authenticated
+        if (user !== '0') $timeout(deferred.resolve);
+
+        // Not Authenticated
+        else {
+          $timeout(deferred.reject);
+          $location.url('/login');
+        }
+      });
+
+      return deferred.promise;
+    };
+
+
 
     // states for my app
     $stateProvider
@@ -56,6 +77,13 @@ angular.module('mean.users').config(['$stateProvider',
         templateUrl: 'users/views/reset-password.html',
         resolve: {
           loggedin: checkLoggedOut
+        }
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'users/views/edit-profile.html',
+        resolve: {
+          loggedin: checkLoggedin
         }
       });
   }
