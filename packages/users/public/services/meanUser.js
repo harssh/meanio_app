d30ecc100ed7a6d70d6950f4af150e2bd5f743e7
harssh <harsshs@sparkway.com>
2014-11-07 17:42:38 +0530

    Added factory for mean user

--- packages/users/public/services/meanUser.js
+++ packages/users/public/services/meanUser.js
@@ -1,10 +1,25 @@
 'use strict';
 
-angular.module('mean.users').factory('MeanUser', [
+angular.module('mean.users')
+	.factory('MeanUser', ['$resource',
 
-  function() {
-    return {
-      name: 'users'
-    };
-  }
-]);
+	 function($resource) {
+			return $resource('users', {}, {
+				update: {
+					method: 'PUT'
+				}
+			});
+	  }
+	])
+	.factory('Authentication', [
+
+	function() {
+		var _this = this;
+
+		_this._data = {
+			user: window.user
+		};
+
+		return _this._data;
+		}
+	]);
